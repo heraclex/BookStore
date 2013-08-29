413a36cd74e48ce3db4df933c8a6074906a6d9f0
heraclex <toan.le.group@gmail.com>
2013-08-29 06:51:15 -0700

    clean up code

--- OJb_BookStore/DataModules/Product/Ojb.DataModules.Product.Mapping/Mapping/CategoryMapping.cs
+++ OJb_BookStore/DataModules/Product/Ojb.DataModules.Product.Mapping/Mapping/CategoryMapping.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Ojb.DataModules.Product.Mapping.Mapping
+﻿namespace Ojb.DataModules.Product.Mapping.Mapping
 {
     public class CategoryMapping
     {
