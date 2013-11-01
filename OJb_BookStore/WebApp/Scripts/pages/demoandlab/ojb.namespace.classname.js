f124bc057372e6f2f369b38de27bf49bd9d8a06c
Toan Le (Never land) <toan.le.group@gmail.com>
2013-11-01 00:05:37 -0700

    refactor

---
+++ OJb_BookStore/WebApp/Scripts/pages/demoandlab/ojb.namespace.classname.js
@@ -0,0 +1,50 @@
+﻿;
+(function ($, Ojb, undefined) {
+    Ojb.NameSpace = function () { };
+    Ojb.NameSpace.prototype = {
+        
+        Name: 'Ojb.NameSpace',
+        
+        getType: function () {
+            return this.Name;
+        }
+    };
+    
+})(jQuery, window.Ojb = window.Ojb || {});
+
+;
+(function ($, Ojb, undefined) {
+
+    //Ojb.NameSpace.ClassName1 = function () { };
+    //Ojb.NameSpace.ClassName1.prototype = {
+    //    base: new Ojb.NameSpace(),
+    //    Name: 'ClassName1',
+
+    //    getType: function () {
+    //        this.base.getType();
+    //    }
+    //};
+    
+    Ojb.NameSpace.ClassName1 = function () { };
+    Ojb.NameSpace.ClassName1.prototype = {
+        base: new Ojb.NameSpace(),
+
+        init: function () {
+            alert('Ojb.NameSpace.ClassName1');
+            alert(this.base.getType());
+        }
+    };
+
+})(jQuery, window.Ojb = window.Ojb || {});
+
+;
+(function ($, Ojb, undefined) {
+    Ojb.NameSpace.ClassName1.Children = {
+        base: new Ojb.NameSpace.ClassName1(),
+
+        init: function () {
+            alert(this.base.getType());
+        }
+    };
+
+})(jQuery, window.Ojb = window.Ojb || {});
---
+++ OJb_BookStore/WebApp/Scripts/pages/login/ojb.login.js
@@ -0,0 +1,47 @@
+﻿;
+(function ($, Ojb, undefined) {
+    Ojb.Login = {
+        list1Subscribed: false,
+        list2Subscribed: false,
+
+        init: function () {
+            Ojb.Login.registerEvent();
+        },
+
+        registerEvent: function () {
+            $('#List1').change(Ojb.Login.list1Change);
+            $('#List2').change(Ojb.Login.list2Change);
+        },
+
+        list1Change: function () {
+            if (Ojb.Login.list2Subscribed) {
+                var value2 = $('#List2').val();
+                $('#List1').val(value2);
+                Ojb.Login.list2Subscribed = false;
+                return;
+            }
+            
+            Ojb.Login.list1Subscribed = true;
+            var value = $('#List1').val();
+            
+            // triger to List 2 if List 1 change
+            $('#List2').val(value).trigger('change');
+        },
+        
+        list2Change: function () {
+            if (Ojb.Login.list1Subscribed) {
+                var value1 = $('#List1').val();
+                $('#List2').val(value1);
+                Ojb.Login.list1Subscribed = false;
+                return;
+            }
+            
+            Ojb.Login.list2Subscribed = true;
+            var value2= $('#List2').val();
+
+            // triger to List 1 if List 2 change
+            $('#List1').val(value2).trigger('change');
+        },
+
+    };
+})(jQuery, window.Ojb = window.Ojb || {});
